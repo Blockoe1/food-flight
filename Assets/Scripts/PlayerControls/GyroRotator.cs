9365339030f2ada5d7da245c74bdb3240e542a1f
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-28 14:48:00 -0600

    Rotation FIxes

--- Assets/Scripts/PlayerControls/GyroRotator.cs
+++ Assets/Scripts/PlayerControls/GyroRotator.cs
@@ -35,6 +35,8 @@ namespace FoodFlight
             {
                 // Read Gyro input and update the TargetRotation.
                 Vector3 gyroVector = inSync.GetAccumulatedGyro();
+                // Swap the Y and Z gyro to account for straigt down.
+                (gyroVector.y, gyroVector.z) = (gyroVector.z, gyroVector.y);
                 Vector3 processedGyro = IgnoreThreshold(gyroVector, gyroThreshold) * gyroSensitivity;
                 Quaternion gyroQuat = Quaternion.Euler(processedGyro);
                 targetRotation = targetRotation * gyroQuat;
