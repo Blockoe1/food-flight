443bb0bb48da36ffbf23a8b048ffa06d0500939e
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-31 15:20:12 -0600

    InputActions Updatae

--- Assets/Scripts/PlayerControls/InputSynchronizer.cs
+++ Assets/Scripts/PlayerControls/InputSynchronizer.cs
@@ -89,7 +89,8 @@ namespace FoodFlight
         private void SetSyncState(int jslIndex, InputDevice inputDevice)
         {
             sync = new InputSyncState(jslIndex, inputDevice);
-            playerInput.SwitchCurrentControlScheme(inputDevice);
+            // Also pass the main keyboard to this player's input.
+            playerInput.SwitchCurrentControlScheme(inputDevice, InputSystem.GetDevice<Keyboard>());
             Debug.Log(playerInput.currentControlScheme);
         }
 
