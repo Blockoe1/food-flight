1edf726ca505f02862f2a089dc2f40a01a03a68b
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-02-09 13:50:38 -0600

    Ragdoll Fix

--- Assets/Scripts/Ragdoll/RagdollController.cs
+++ Assets/Scripts/Ragdoll/RagdollController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace FoodFlight
@@ -10,7 +11,9 @@ namespace FoodFlight
         private Rigidbody[] rigidBodies;
         private Animator animator;
 
-        void Start()
+        private Coroutine ragdollRoutine;
+
+        void Awake()
         {
             rigidBodies = GetComponentsInChildren<Rigidbody>();
             animator = GetComponent<Animator>();
@@ -43,6 +46,8 @@ namespace FoodFlight
             }
 
             Vector3 originalHipsPos = hips.position;
+            transform.parent.position = hips.position;
+            hips.position = originalHipsPos;
             transform.position = hips.position;
             hips.position = originalHipsPos;
 
@@ -61,6 +66,27 @@ namespace FoodFlight
             }
 
             animator.enabled = false;
+        }
+
+        /// <summary>
+        /// Causes the player to ragdoll for a certain number of seconds.
+        /// </summary>
+        /// <param name="seconds"></param>
+        public void RagdollForSeconds(float seconds)
+        {
+            if (ragdollRoutine != null)
+            {
+                StopCoroutine(ragdollRoutine);
+                ragdollRoutine = null;
+            }
+            ragdollRoutine = StartCoroutine(RagdollRoutine(seconds));
+        }
+        private IEnumerator RagdollRoutine(float seconds)
+        {
+            EnableRagdoll();
+            yield return new WaitForSeconds(seconds);
+            DisableRagdoll();
+            ragdollRoutine = null;
         }
     }
 }
